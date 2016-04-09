511d7daa399fc39721987a147f91166b5901739a
Shin Yamagami <kiboh.usa@gmail.com>
2016-04-08 20:41:24 -0700

    upload

---
+++ chapter.1/Exercises/06/Meta.java
@@ -0,0 +1,9 @@
+public class Meta{
+    public static void main(String[] args){
+        System.out.println("public class Meta{\n" +
+                            "\tpublic static void main(String[] args){\n" +
+                                   "\t\tSystem.out.println(\"Hello, world!\")\n" +
+                               "\t}\n" +
+                            "}");
+    }
+}
