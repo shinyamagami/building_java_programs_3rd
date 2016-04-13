b52768430d9006e9a8ffcb5318e68851c4726b82
Shin Yamagami <kiboh.usa@gmail.com>
2016-04-12 17:44:17 -0700

    upload

---
+++ chapter.2/Exercises/13/Loops.java
@@ -0,0 +1,12 @@
+public class Loops{
+	public static void main(String[] args){
+		for (int i = 1; i <= 5; i++){
+			for(int j = 9; j >= 0; j--){
+				for(int k = 1; k <= 5; k++){
+					System.out.print(j);
+				}
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ chapter.2/Exercises/14/Loops.java
@@ -0,0 +1,12 @@
+public class Loops{
+	public static void main(String[] args){
+		for (int i = 1; i <= 4; i++){
+			for(int j = 9; j >= 0; j--){
+				for(int k = 1; k <= j; k++){
+					System.out.print(j);
+				}
+			}
+			System.out.println();
+		}
+	}
+}
