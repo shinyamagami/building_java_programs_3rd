35514c9c39b9461bd941364db8dc43d49630fbe8
Shin Yamagami <kiboh.usa@gmail.com>
2016-04-21 20:51:38 -0700

    upload

---
+++ chapter.03/Exercises/22/CollegeAdmit.java
@@ -0,0 +1,28 @@
+/**
+ * Couldn't solve this without doing if/else
+
+
+
+import java.util.*; // for Scanner
+
+public class CollegeAdmit{
+    public static void collegeAdmit(){
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("University admission program!");
+        System.out.print("What is your GPA?");
+        double gpa = console.nextDouble();
+        System.out.print("What is your SAT score?");
+        int score = console.nextInt();
+
+        for (double i = gpa; i <= 9; i += 10){
+            System.out.println("You were rejected!")
+            for
+        }
+
+    }
+    public static void main(String[] args){
+        collegeAdmit();
+    }
+}
+*/
