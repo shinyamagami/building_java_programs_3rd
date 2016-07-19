327a8fb4bcdb81ae4261a72e48148be210157475
Shin Yamagami <kiboh.usa@gmail.com>
2016-07-18 22:35:00 -0700

    update

--- chapter.06/Exercises/04/CointCoins.java
+++ chapter.06/Exercises/04/CointCoins.java
@@ -1,29 +1,22 @@
-import java.io.*;
-import java.util.*;
-
-public class CointCoins{
-    public static void cointCoins(Scanner input){
-        double sum = 0;
-        while(input.hasNext()){
-            int number = input.nextInt();
-            String type = input.next().toLowerCase();
-            System.out.println(number + " " + type);
-            if (type.equals("pennies")){
-                sum = sum + number * 0.01;
-            } else if (type.equals("nickels")){
-                sum = sum + number * 0.05;
-            } else if (type.equals("dimes")){
-                sum = sum + number * 0.10;
-            } else if (type.equals("quarters")){
-                sum = sum + number * 0.25;
-            }
+public void countCoins(Scanner input){
+	double pennies  = 0.01;
+    double nickels  = 0.05;
+    double dimes	= 0.10;
+    double quarters = 0.25;
+    double sum = 0.00;
+    while(input.hasNext() == true){
+        int num = input.nextInt();
+        String currency = input.next();
+        currency = currency.toLowerCase();
+        if(currency.equals("pennies")){
+            sum += (num * pennies);
+        } else if(currency.equals("nickels")){
+            sum += (num * nickels);
+        } else if(currency.equals("dimes")){
+            sum += (num * dimes);
+        } else {
+            sum += (num * quarters);
         }
-        System.out.printf("Total money: $%.2f",sum);
-        System.out.println();
     }
-    public static void main(String[] args)
-    throws FileNotFoundException{
-        Scanner input = new Scanner(new File("coins.txt"));
-        cointCoins(input);
-    }
+    System.out.printf("Total money: $%.2f", sum);
 }
