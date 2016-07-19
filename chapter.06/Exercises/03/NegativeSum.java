7f0faa6f013c04675f750b0526b7a7400155bda5
Shin Yamagami <kiboh.usa@gmail.com>
2016-07-18 20:52:30 -0700

    update

--- chapter.06/Exercises/03/NegativeSum.java
+++ chapter.06/Exercises/03/NegativeSum.java
@@ -1,31 +1,18 @@
-import java.io.*;
-import java.util.*;
-
-public class NegativeSum{
-    public static boolean negativeSum(Scanner console)
-    throws FileNotFoundException{
-        Scanner input = new Scanner(new File("integers.txt"));
-        int sum = 0;
-        int count = 0;
-        boolean result = false;
-        while (input.hasNext() == true){
-            if (sum >= 0){
-                sum += input.nextInt();
-                count++;
-            } else {
-                result = true;
-            }
-        }
-        if (result == false){
-            System.out.println("no negative sum");
-        } else {
-            System.out.println("sum of " + sum + "after " + count + " steps");
-        }
-        return result;
-    }
-    public static void main(String[] args)
-    throws FileNotFoundException{
-        Scanner console = new Scanner(System.in);
-        negativeSum(console);
-    }
+public boolean negativeSum(Scanner input){
+	int num = input.nextInt();
+    int sum = num;
+    int count = 1;
+    boolean result = true;
+	while(sum >= 0 && input.hasNextInt() == true){
+        count++;
+        num = input.nextInt();
+        sum += num;
+    }
+    if(sum >= 0){
+        result = false;
+        System.out.println("no negative sum");
+    } else {
+        System.out.println(sum + " after " + count + " steps");
+    }
+    return result;
 }
